0ef28920a176a8c04781a4c0234ec34d54051fd1
Joel <joelmik123@gmail.com>
2021-09-14 11:58:51 +0200

    header day 2

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Image from "next/image";
 
 import { SearchIcon, 
@@ -7,11 +9,14 @@ import { SearchIcon,
     } from "@heroicons/react/solid";
 
 function Header() {
+
+    const [searchInput, setSearchInput] = useState("");
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             
             {/* Left */}
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div className="relative flex items-center h-8 cursor-pointer my-auto">
                 <Image 
                     src="https://links.papareact.com/qd3"
                     layout="fill"
@@ -22,7 +27,13 @@ function Header() {
 
             {/* Middle */}
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input type="text" placeholder="Start your search" className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400"/>
+                <input 
+                    value={searchInput} 
+                    onChange={(e)=> setSearchInput(e.target.value)} 
+                    type="text" 
+                    placeholder="Start your search" 
+                    className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400"
+                />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
             </div>
 
